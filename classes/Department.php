0a4c57fc758d4d1ba052322f97f9c814fbc718d2
Erik Berndt <erikberndt@gmx.net>
2023-04-21 14:28:13 +0200

    add class Department incl. constructor and getters

--- classes/Department.php
+++ classes/Department.php
@@ -2,5 +2,33 @@
 
 class Department
 {
+  private int $id;
+  private string $name;
+
+  /**
+   * @param int $id
+   * @param string $name
+   */
+  public function __construct(int $id, string $name)
+  {
+    $this->id = $id;
+    $this->name = $name;
+  }
+
+  /**
+   * @return int
+   */
+  public function getId(): int
+  {
+    return $this->id;
+  }
+
+  /**
+   * @return string
+   */
+  public function getName(): string
+  {
+    return $this->name;
+  }
 
 }
