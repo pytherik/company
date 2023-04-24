96b7b6e917e454a113a28db9e3d3dc46b09593ff
Erik Berndt <erikberndt@gmx.net>
2023-04-24 12:18:50 +0200

    add btn style

--- views/navigation.php
+++ views/navigation.php
@@ -1,11 +1,14 @@
 <nav>
+  <a href="index.php?action=showList&area=employee">
+    <button class="btn">Liste Mitarbeiter</button>
+  </a>
   <a href="index.php?action=showCreate&area=employee">
-    <button class="btn">MA erstellen</button>
-    <a href="index.php?action=showCreate&area=department">
-      <button class="btn">Department Erstellen</button>
-      <a href="index.php?action=showList&area=department">
-        <button class="btn">Liste Abteilungen</button>
-        <a href="index.php?action=showList&area=employee">
-          <button class="btn">Liste Mitarbeiter</button>
-        </a>
+    <button class="btn create">MA erstellen</button>
+  </a>
+  <a href="index.php?action=showList&area=department">
+    <button class="btn">Liste Abteilungen</button>
+  </a>
+  <a href="index.php?action=showCreate&area=department">
+    <button class="btn create">Abteilung Erstellen</button>
+  </a>
 </nav>
