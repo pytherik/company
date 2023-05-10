3866ed6b9f2b2e6b202501a78758a6ada01364da
pytherik <erikberndt@gmx.net>
2023-05-10 17:13:39 +0200

    index aufgeräumt

--- index.php
+++ index.php
@@ -6,6 +6,14 @@ spl_autoload_register(function ($class) {
   include 'classes/' . $class . '.php';
 });
 
+if (PERSISTENCY === 'file') {
+  $EmployeeClass = 'EmployeeFile';
+  $DepartmentClass = 'DepartmentFile';
+} else {
+  $EmployeeClass = 'EmployeeDb';
+  $DepartmentClass = 'DepartmentDb';
+}
+
 $action = $_REQUEST['action'] ?? 'showList';
 $area = $_REQUEST['area'] ?? 'employee';
 $id = $_REQUEST['id'] ?? '';
@@ -21,126 +29,63 @@ try {
   switch ($action) {
     case 'showList':
       if ($area === 'employee') {
-        if (PERSISTENCY === 'file') {
-          $employees = (new EmployeeFile())->getAllAsObjects();
-        } else {
-          $employees = (new EmployeeDb())->getAllAsObjects();
-        }
+          $employees = (new $EmployeeClass())->getAllAsObjects();
       } else if ($area === 'department') {
-        if (PERSISTENCY === 'file') {
-          $departments = (new DepartmentFile())->getAllAsObjects();
-        } else {
-          $departments = (new DepartmentDb())->getAllAsObjects();
-        }
-//        $departments = (new DepartmentFile())->getFromDatabase();
+          $departments = (new $DepartmentClass())->getAllAsObjects();
       }
       $view = $action;
       break;
     case 'showUpdate':
       if ($area === 'employee') {
-        if (PERSISTENCY === 'file') {
-          $employee = (new EmployeeFile())->getObjectById($id);
-        } else {
-          $employee = (new EmployeeDb())->getObjectById($id);
-        }
-        if (PERSISTENCY === 'file') {
-          $departments = (new DepartmentFile())->getAllAsObjects();
-        } else {
-          $departments = (new DepartmentDb())->getAllAsObjects();
-        }
+          $employee = (new $EmployeeClass())->getObjectById($id);
+          $departments = (new $DepartmentClass())->getAllAsObjects();
       } else if ($area === 'department') {
-        if (PERSISTENCY === 'file') {
-          $department = (new DepartmentFile())->getObjectById($id);
-        } else {
-          $department = (new DepartmentDb())->getObjectById($id);
-        }
+          $department = (new $DepartmentClass())->getObjectById($id);
       }
       $activity = 'bearbeiten';
       $view = 'showUpdateAndCreate';
       break;
     case 'showCreate':
       if ($area === 'employee') {
-        if (PERSISTENCY === 'file') {
-          $departments = (new DepartmentFile())->getAllAsObjects();
-        } else {
-          $departments = (new DepartmentDb())->getAllAsObjects();
-        }
+          $departments = (new $DepartmentClass())->getAllAsObjects();
       }
       $activity = 'erstellen';
       $view = 'showUpdateAndCreate';
       break;
     case 'delete':
       if ($area === 'employee') {
-        if (PERSISTENCY === 'file') {
-          (new EmployeeFile())->delete($id);
-          $employees = (new EmployeeFile())->getAllAsObjects();
-        } else {
-          (new EmployeeDb())->delete($id);
-          $employees = (new EmployeeDb())->getAllAsObjects();
-        }
+          (new $EmployeeClass())->delete($id);
+          $employees = (new $EmployeeClass())->getAllAsObjects();
       } else if ($area === 'department') {
-        if (PERSISTENCY === 'file') {
-          (new DepartmentFile())->delete($id);
-          $departments = (new DepartmentFile())->getAllAsObjects();
-        } else {
-          (new DepartmentDb())->delete($id);
-          $departments = (new DepartmentDb())->getAllAsObjects();
+          (new $DepartmentClass())->delete($id);
+          $departments = (new $DepartmentClass())->getAllAsObjects();
         }
-      }
       $view = 'showList';
       break;
     case 'update':
       if ($area === 'employee') {
-        if (PERSISTENCY === 'file') {
-          $employee = new EmployeeFile($id, $firstName, $lastName, $departmentId);
+          $employee = new $EmployeeClass($id, $firstName, $lastName, $departmentId);
           $employee->updateObject();
-          $employees = (new EmployeeFile())->getAllAsObjects();
-        } else {
-          $employee = new EmployeeDb($id, $firstName, $lastName, $departmentId);
-          $employee->updateObject();
-          $employees = (new EmployeeDb())->getAllAsObjects();
-        }
+          $employees = (new $EmployeeClass())->getAllAsObjects();
       } else if ($area === 'department') {
-        if (PERSISTENCY === 'file') {
-          $department = new DepartmentFile($id, $departmentName);
+          $department = new $DepartmentClass($id, $departmentName);
           $department->updateObject();
-          $departments = (new DepartmentFile())->getAllAsObjects();
-        } else {
-          $department = new DepartmentDb($id, $departmentName);
-          $department->updateObject();
-          $departments = (new DepartmentDb())->getAllAsObjects();
-        }
+          $departments = (new $DepartmentClass())->getAllAsObjects();
       }
       $view = 'showList';
       break;
     case 'create':
       if ($area === 'employee') {
-        if (PERSISTENCY === 'file') {
-          (new EmployeeFile())->createNewObject($firstName, $lastName, $departmentId);
-        } else {
-          (new EmployeeDb())->createNewObject($firstName, $lastName, $departmentId);
-        }
+          (new $EmployeeClass())->createNewObject($firstName, $lastName, $departmentId);
       } else if ($area === 'department') {
-        if (PERSISTENCY === 'file') {
-          (new DepartmentFile())->createNewObject($departmentName);
-        } else {
-          (new DepartmentDb())->createNewObject($departmentName);
-        }
+          (new $DepartmentClass())->createNewObject($departmentName);
       }
     default:
       // falls unerwarteter Wert für $action übergeben wird
       if ($area === 'employee') {
-        if (PERSISTENCY === 'file') {
-          $employees = (new EmployeeFile())->getAllAsObjects();
-        } else {
-          $employees = (new EmployeeDb())->getAllAsObjects();
-        }
+          $employees = (new $EmployeeClass())->getAllAsObjects();
       } else if ($area === 'department') {
-        if (PERSISTENCY === 'file') {
-          $departments = (new DepartmentFile())->getAllAsObjects();
-        } else {
-          $departments = (new DepartmentDb())->getAllAsObjects();
-        }
+          $departments = (new $DepartmentClass())->getAllAsObjects();
       }
       $view = 'showList';
       break;
